7a4af8ba540e640f89d412f40564fe5899271c3f
Alexandr Svistunov <alsvi2013@yandex.ru>
2020-01-24 17:07:59 +0500

    form end contacts
    Оживление формы и подключение странички с контактами

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,83 @@
-var burgerMenuItem = document.querySelector('.burger-item');
+$(document).ready(function () {
 
-burgerMenuItem.onclick = function () {
-	var MenuBurger = document.querySelector('.burger-lines');
-	var mobileNav = document.querySelector('.mobile-nav');
+	var burgerMenuItem = document.querySelector('.burger-item');
 
-	MenuBurger.classList.toggle('burger-active');
-	mobileNav.classList.toggle('mobile-nav-active');
+	burgerMenuItem.onclick = function () {
+		var MenuBurger = document.querySelector('.burger-lines');
+		var mobileNav = document.querySelector('.mobile-nav');
 
-};
+		MenuBurger.classList.toggle('burger-active');
+		mobileNav.classList.toggle('mobile-nav-active');
 
-$('.filter-projects').mixItUp();
+	};
+	//-фильтрация работ
+	$('.filter-projects').mixItUp();
+
+	//-fake-placeholder
+	let validateInputs = document.querySelectorAll('.input-validate');
+	for (let i = 0; i < validateInputs.length; i++) {
+		validateInputs[i].addEventListener('change', function () {
+
+			if (this.value.length !== '0') {
+				this.nextElementSibling.classList.add('active')
+			} else {
+				this.nextElementSibling.classList.remove('active')
+			}
+		})
+	}
+	//-validate form
+	$('#contact-form').validate({
+		rules: {
+			email: {
+				required: true,
+				email: true
+			},
+			theme: {
+				required: true
+			},
+			message: {
+				required: true
+			}
+		},
+		messages: {
+			email: {
+				required: 'Введите Ваш Email',
+				email: 'Отсутствует символ @'
+			},
+			theme: {
+				required: 'Введите тему сообщения'
+			},
+			message: {
+				required: 'Введите текст сообщения'
+			}
+		},
+
+		submitHandler: function (form) {
+			ajaxFormSubmit();
+		}
+	});
+
+	// Функция AJAX запрса на сервер
+	function ajaxFormSubmit() {
+		let string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку. 
+
+		// Формируем ajax запрос
+		$.ajax({
+			type: "POST", // Тип запроса - POST
+			url: "php/mail.php", // Куда отправляем запрос
+			data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+			// Функция если все прошло успешно
+			success: function (html) {
+				$("#contact-form").slideUp(800);
+				$('#answer').html(html);
+			}
+		});
+
+		// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+		return false;
+	}
+
+
+
+})
